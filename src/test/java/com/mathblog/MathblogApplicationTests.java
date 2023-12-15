5e314b3773f7f8c9dff99613e05325b6de247ddd
bjk930 <kokkohkof@naver.com>
2023-12-15 22:30:06 +0900

    first

---
+++ src/test/java/com/mathblog/MathblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
